a26470f253cd6b1a9c0544102582f83383fb8211
321-bip <79604277+321-bip@users.noreply.github.com>
2021-08-30 22:39:28 -0300

    22:39

---
+++ index.js
@@ -0,0 +1,29 @@
+ let pos = 1;
+ 
+ function precaregar()
+  {
+    let imagem = document.getElementById("imagem")
+        imagem.setAttribute("class","img" + pos)
+  }
+ 
+ function slider()
+  {
+    precaregar() 
+    if(pos < 7)
+     {
+       pos++
+     }
+    else
+     {
+       pos = 1
+     }
+}
+
+function man()
+ {
+  setInterval(slider,1300)
+ }
+ man()
+
+
+

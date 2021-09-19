2275b243449ce9c950289773e8c69077b1813f8a
321-bip <79604277+321-bip@users.noreply.github.com>
2021-09-19 16:11:40 -0300

    dddd

--- indexcontatos.js
+++ indexcontatos.js
@@ -7,18 +7,19 @@ function mensagemUsuario(){
         icon: "success",
       });
 
-      redireciona()
+    //  redireciona()
 }
-     document.getElementById("area-enviar").addEventListener("click",mensagemUsuario)
-            //  setinterval(mensagemUsuario(),5000)
+     document.getElementById("area-enviar").addEventListener("click",redireciona)
+              
 
 function redireciona(){
 
-    let link = "https://321-bip.github.io/321-bip-portfolio/indexcontatos"
+    let link = "https://321-bip.github.io/321-bip-portfolio/"
     let input4 = document.getElementById("capitura").elements[4]
         input4.setAttribute("value",link)
+        
 }   
-    //setTimeout(redireciona,3000) 
+    
 //redireciona()
 
 console.log(document.getElementById("capitura").elements[4])

715caef8420bed4364cef10f8343318d44521d53
321-bip <79604277+321-bip@users.noreply.github.com>
2021-09-20 21:32:45 -0300

    hhhh

--- indexcontatos.js
+++ indexcontatos.js
@@ -1,25 +1,16 @@
 
 function mensagemUsuario(){
-   
-    swal({
-        title: document.getElementById("capitura").elements[1].value ,
-        text: "Email enviado com suseso",
-        icon: "success",
-      });
 
-    //  redireciona()
+    let nomeContato = document.getElementById("capitura").elements[1].value
+    alert(`${nomeContato} Email Enviado com susesso!!!!`);
 }
-     document.getElementById("area-enviar").addEventListener("click",redireciona)
+   document.getElementById("area-enviar").addEventListener("click",redireciona)
               
-
 function redireciona(){
 
     let link = "https://321-bip.github.io/321-bip-portfolio/"
     let input4 = document.getElementById("capitura").elements[4]
         input4.setAttribute("value",link)
-        
+        mensagemUsuario() 
 }   
     
-//redireciona()
-
-console.log(document.getElementById("capitura").elements[4])
